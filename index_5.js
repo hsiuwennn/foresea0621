f4c9a9aed1fd7b1b209cdf969e506b9f4e8f2cc6
hsiuwennn <44960564+hsiuwennn@users.noreply.github.com>
2020-06-21 21:10:12 +0800

    new

---
+++ index_5.js
@@ -0,0 +1,52 @@
+const root = document.getElementById('root');
+const content = document.getElementById('content');
+
+window.addEventListener('load', () =>{
+  const preload=document.querySelector('.preload');
+  preload.classList.add('preload-finish');
+
+});
+
+root.addEventListener('wheel', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  
+  const delta = e.deltaY;
+  
+  const maxScrollY = content.offsetHeight - root.offsetHeight;
+  const maxScrollX = content.offsetWidth - root.offsetWidth;
+
+  
+  let scrollY = root.scrollTop;
+  let scrollX = root.scrollLeft;
+  
+  if (scrollX > 0) {
+    scrollX += delta;
+    
+    if (scrollX < 0) {
+      scrollY -= scrollX;
+      scrollX = 0;
+    }
+  } else {
+    scrollY += delta;
+  
+    const overflow = scrollY - maxScrollY;
+    if (overflow > 0) {
+      scrollX += overflow;
+      scrollY = maxScrollY;
+    } else {
+      scrollX = 0;
+    }
+  }
+  
+  root.scrollTo(scrollX, scrollY);
+});
+
+$(function(){
+   AOS.init();
+})
+
+
+ 
+
+
